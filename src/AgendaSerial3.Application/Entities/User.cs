9be2866ab0ce3c94ace8c36bf3eb2efed74cde72
Felipe Braga Gomes <bragagomesfelipe@outlook.com>
2025-06-26 10:35:34 -0300

    Adicionar projeto AgendaSerial3.Application e entidades
    Criação do projeto `AgendaSerial3.Application` com referências aos projetos `AgendaSerial3.Domain` e `AgendaSerial3.Infrastructure`. Atualização do arquivo de projeto para o SDK do .NET e configuração para `net9.0`, com usabilidade implícita e suporte a tipos nulos.
    
    Adição da classe `Appointment` com propriedades relevantes para compromissos, e introdução da classe `PersonalCalendar`, que contém informações sobre calendários pessoais e suas coleções de compromissos.
    
    Expansão da classe `User`, que agora inclui propriedades como `FirstName`, `LastName`, `CreatedDate`, além de coleções de `PersonalCalendars` e `Appointments`.

---
+++ src/AgendaSerial3.Application/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AgendaSerial3.Application.Entities;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    public virtual ICollection<PersonalCalendar> PersonalCalendars { get; set; } = [];
+    public virtual ICollection<Appointment> Appointments { get; set; } = [];
+}
